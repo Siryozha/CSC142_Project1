cc2d60fddf36100183cb77b589ff7cfb5cf1a620
AaronHand <spottydj@gmail.com>
2016-04-19 15:26:26 -0700

    changes to grid

--- src/snowman/Grid.java
+++ src/snowman/Grid.java
@@ -10,6 +10,14 @@ public class Grid {
     private int width,height,xPt,yPt;
     private Point[][] points;
 
+
+    /**
+     *
+     * @param xPt location of Object
+     * @param yPt location of Object
+     * @param width width of Object
+     */
+
     public Grid(int xPt, int yPt, int width) {
         this.width = width;
         this.height = width * 2;
@@ -20,22 +28,24 @@ public class Grid {
 
     private void setPoints(){
 
-        points = new Point[11][21];
-        for (int i = 0; i <= 10 ; i++) {
+        points = new Point[11][21];                 //grid size 11x21 allows for center
+
+        for (int i = 0; i <= 10 ; i++) {            //populate grid with scaled cells
             for (int j = 0; j <= 20 ; j++) {
-                System.out.println(i + " - " + j);
                 points[i][j] = new Point(
                         i * (width / 10) + xPt,
                         j * (height / 20) + yPt
                 );
-                System.out.println(points[i][j]);
             }
-
         }
-
-
-
     }
+
+    /**
+     *
+     * @param x location
+     * @param y location
+     * @return Point
+     */
 
     public int getX(int x, int y) {
         return (int)points[x][y].getX();
@@ -43,6 +53,11 @@ public class Grid {
     public int getY(int x, int y) {
         return (int)points[x][y].getY();
     }
+
+    /**
+     * Return single cell width or height
+     * Used for padding
+     */
 
     public int spanX(int x){
         return x * (width / 10);
